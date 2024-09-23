0df66b59a08ae48039f32b70dc19d86566c45a26
Sarah <spritchett@students.mchenry.edu>
2024-09-23 13:12:27 -0500

    renamed folder and fixed button onclick

---
+++ M1_A2 - AddingJavaScriptToHTML/scripts/script.js
@@ -0,0 +1,9 @@
+ function myFunction2(){
+      document.getElementById("sample").innerHTML = "Changed from a function in External Script.";
+}
+    
+// single line comment
+
+/*
+block/ multi-line comment
+*/
