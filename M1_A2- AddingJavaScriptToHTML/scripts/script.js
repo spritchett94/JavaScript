f2d6cb424998abb978624b0ce25efb0609130079
spritchett94 <122915036+spritchett94@users.noreply.github.com>
2024-09-23 13:17:04 -0500

    Delete M1_A2- AddingJavaScriptToHTML directory

--- M1_A2- AddingJavaScriptToHTML/scripts/script.js
+++
@@ -1,9 +0,0 @@
- function myFunction2(){
-      document.getElementById("sample").innerHTML = "Changed from a function in External Script.";
-}
-    
-// single line comment
-
-/*
-block/ multi-line comment
-*/
