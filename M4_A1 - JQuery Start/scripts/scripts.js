7264874fb3c1a671cbb905d1315a282380916d09
Sarah <spritchett@students.mchenry.edu>
2024-11-02 23:35:30 -0500

    first commit

---
+++ M4_A1 - JQuery Start/scripts/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#pgBtn").click(function(){
+       $(".pg").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#divBtn").click(function(){
+          $("#section").hide();
+     });
+});
+
+$(document).ready(function(){
+  $("h2").click(function(){
+    $(this).hide();
+  });
+});
+
+
+
