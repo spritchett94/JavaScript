f0210b12c045b019cb6f6d53bd5021a2c481c8eb
Sarah <spritchett@students.mchenry.edu>
2024-11-03 14:46:52 -0600

    first commit

---
+++ M4_A2 - JQuery Effects/scripts/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $("#img1").show();
+  });
+     $("#btn2").click(function () {
+          $("#img1").hide();
+     })
+});
+
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("#img2").fadeToggle(3000);
+  });
+});
+
+$(document).ready(function(){
+  $("#btn3").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
