41023090ee07b618a05a55871189b693a81d76ba
Sarah <spritchett@students.mchenry.edu>
2024-11-03 21:00:45 -0600

    added chaining, callback, and stop

--- M4_Assessment01/scripts/script.js
+++ M4_Assessment01/scripts/script.js
@@ -2,25 +2,38 @@ $(document).ready(function () {
      
 	$("#up").click(function(){
 		$("#box").animate({
-			marginTop: '-=20px'
-		});
+			marginTop: '-=120px'
+		}, 2000, function () {
+			$("#box").css("background-color", "mediumslateblue");
+		}).css("background-color", "red");
 	});
 
 	$("#down").click(function(){
 		$("#box").animate({
-			marginTop: '+=20px'
-		});
+			marginTop: '+=120px'
+		}, 2000, function () {
+			$("#box").css("background-color", "mediumslateblue");
+		}).css("background-color", "blue");
 	});
 
 	$("#left").click(function(){
 		$("#box").animate({
-			marginLeft: '-=20px'
-		});
+			marginLeft: '-=200px'
+		}, 2000, function () {
+			$("#box").css("background-color", "mediumslateblue");
+		}).css("background-color", "orange");
 	});
 
 	$("#right").click(function(){
 		$("#box").animate({
-			marginLeft: '+=20px'
-		});
+			marginLeft: '+=200px'
+		}, 2000, function () {
+			$("#box").css("background-color", "mediumslateblue");
+		}).css("background-color", "green");
 	});
+
+	$("#stop").click(function(){
+    $("#box").stop();
+  });
+
 });
